dc40436eabb66a9f7d5cd61a00637a0a09ab9434
camilonogales96 <nogales.fuentes.camilo.baltazar@gmail.com>
2024-10-06 03:11:55 -0400

    Generación de tablas BD

---
+++ migrations/20241006070614-create-diagrama.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Diagramas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      imagen: {
+        type: Sequelize.BLOB
+      },
+      nota: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Diagramas');
+  }
+};
